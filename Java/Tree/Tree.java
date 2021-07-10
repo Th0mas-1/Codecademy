bce3c322a752484d18a0da4e5c84dfd94708f9d3
ThomasW <tom.0384@gmail.com>
2021-07-11 01:28:47 +0200

    Tree.java finalized

---
+++ Java/Tree/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+	public static void main(String[]args) {
+		/* This is a multiline
+		comment */
+		System.out.println("I really enjoy learning Java programming");
+		System.out.println("Ready to get my hands dirty!");
+		 System.out.println("  *  ");
+    		System.out.println(" *** ");
+    		System.out.println(" *** ");
+    		System.out.println("  *  ");
+    		System.out.println("  *  ");  
+	}
+}
