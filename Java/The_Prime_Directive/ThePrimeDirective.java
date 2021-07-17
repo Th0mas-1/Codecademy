0647b427ea089592068dd173c6a1bbd4e1427012
ThomasW <tom.0384@gmail.com>
2021-07-18 00:37:08 +0200

    prime

---
+++ Java/The_Prime_Directive/ThePrimeDirective.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ThePrimeDirective {
+
+	ArrayList<Integer> prime = new ArrayList<Integer>();	
+
+	public static void main(String [] args) {
+
+		for (int i = 2; i < 100; i++) {
+			if (i % )
+		}
+	}
+}
