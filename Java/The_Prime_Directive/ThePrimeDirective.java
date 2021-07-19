2430e585ccf32d1a997eeec58a508e041da481ac
ThomasW <tom.0384@gmail.com>
2021-07-20 01:05:23 +0200

    the prime directive finalized

--- Java/The_Prime_Directive/ThePrimeDirective.java
+++ Java/The_Prime_Directive/ThePrimeDirective.java
@@ -6,9 +6,12 @@ public class ThePrimeDirective {
 
 		ArrayList<Integer> prime = new ArrayList<Integer>();
 
-		for (int i = 3; i < 100; i++) {
+		for (int i = 2; i < 10000; i++) {
+			boolean test = true;
 			for (int j = 2; j < i; j++) {
-				if (i%j!=0) prime.add(i);}
+				if (i%j==0) {test=false;}
+			}
+		if (test==true) prime.add(i);
 		}
 		System.out.println(prime);
 	}
