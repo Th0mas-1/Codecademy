d738dceca5cc36b7477f6baefba272f6e3a8460a
ThomasW <tom.0384@gmail.com>
2021-07-19 23:47:42 +0200

    The Prime Directive initiated

--- Java/The_Prime_Directive/ThePrimeDirective.java
+++ Java/The_Prime_Directive/ThePrimeDirective.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 
 public class ThePrimeDirective {
 
-	ArrayList<Integer> prime = new ArrayList<Integer>();	
-
 	public static void main(String [] args) {
 
-		for (int i = 2; i < 100; i++) {
-			if (i % )
+		ArrayList<Integer> prime = new ArrayList<Integer>();
+
+		for (int i = 3; i < 100; i++) {
+			for (int j = 2; j < i; j++) {
+				if (i%j!=0) prime.add(i);}
 		}
+		System.out.println(prime);
 	}
 }
